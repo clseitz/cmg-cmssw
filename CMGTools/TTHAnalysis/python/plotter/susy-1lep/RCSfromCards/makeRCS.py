73b8a72009efe1c617bd244b6c4c32fe092a7e32
claudia seitz <clseitz.physics@gmail.com>
2015-08-27 16:58:41 +0200

    add more options for tables

--- CMGTools/TTHAnalysis/python/plotter/susy-1lep/RCSfromCards/makeRCS.py
+++ CMGTools/TTHAnalysis/python/plotter/susy-1lep/RCSfromCards/makeRCS.py
@@ -11,7 +11,7 @@ from multiprocessing import Pool
 from ROOT import *
 import math
 
-def _getYieldsFromInput(inargs):
+def _getYieldsFromInput(inargs, QCDoff = True):
 
     (sample, cardDir, binName, ratDict) = inargs
 
@@ -57,6 +57,9 @@ def _getYieldsFromInput(inargs):
     hQCD = cardf.Get("x_QCD")
     nQCD = hQCD.Integral()
     nQCDErr = hQCD.GetBinError(1)
+    if QCDoff:
+        nQCD = 0
+        nQCDErr=0
 
     hDY = cardf.Get("x_DY")
     nDY = hDY.Integral()
@@ -70,7 +73,7 @@ def _getYieldsFromInput(inargs):
     return (binName,[nTT, nTTErr, nTT_SemiLep, nTTErr_SemiLep, nTT_DiLep, nTTErr_DiLep, nTT_FullHad, nTTErr_FullHad,nSingleT, nSingleTErr, nTTV, nTTVErr, nWJets,  nWJetsErr, nQCD, nQCDErr, nDY, nDYErr])
 
 def makeTable(yieldDict, yieldDict2, format = "text", option = "CRSR"):
-
+    tableAll ={}
     # sort by bin name
     ykeys = sorted(yieldDict.keys())
     #print 'KEYS', sorted(ykeys)
@@ -139,6 +142,7 @@ def makeTable(yieldDict, yieldDict2, format = "text", option = "CRSR"):
 
             allbkgNoTT2 = nSingleT2 + nTTV2 + nWJets2 + nQCD2 + nDY2
             allbkgErrNoTT2 = math.sqrt(nSingleTErr2*nSingleTErr2 +nTTVErr2*nTTVErr2+ nWJetsErr2*nWJetsErr2 +nQCDErr2*nQCDErr2+nDYErr2*nDYErr2)
+            binName = bin
             bin = bin.replace('_', ' $;$ ')
 
             RCSall = 0
@@ -147,6 +151,7 @@ def makeTable(yieldDict, yieldDict2, format = "text", option = "CRSR"):
             if option == "CRSR":
 
                 print "%s & %2.2f & %2.2f &%2.2f  & %2.2f  & %2.2f  & %2.2f  & %2.2f  & %2.2f & %2.4f  \\\ " % ( bin, nTT_DiLep,nTT_SemiLep, allbkgNoTT, allbkg, nTT_DiLep2,nTT_SemiLep2, allbkgNoTT2, allbkg2, RCSall)
+                tableAll[binName] =[nTT_DiLep,nTT_SemiLep, allbkgNoTT, allbkg, nTT_DiLep2,nTT_SemiLep2, allbkgNoTT2, allbkg2, RCSall]
 #            print "%s & %2.2f & %2.2f &%2.2f  & %2.2f  & %2.2f  & %2.2f  & %2.2f  & %2.2f  \\\ " % ( bin, nTT_DiLep,nTT_SemiLep, nSingleT,  nTTV, nWJets,  nQCD,  nDY, allbkgNoTT)
             RCS = 0
             if (nTT_SemiLep+nTT_DiLep)>0:
@@ -168,9 +173,9 @@ def makeTable(yieldDict, yieldDict2, format = "text", option = "CRSR"):
         print"\\end{center}"
 
         print "\\end{table}"
+        
 
-
-    return 1
+    return tableAll 
 
 def makeRCS(yieldDictSR, yieldDictCR):
     RCSval={}
@@ -183,23 +188,11 @@ def makeRCS(yieldDictSR, yieldDictCR):
         
         (nTTCR, nTTCRErr, nTTCR_SemiLep, nTTCRErr_SemiLep, nTTCR_DiLep, nTTCRErr_DiLep, nTTCR_FullHad, nTTCRErr_FullHad, nSingleTCR, nSingleTCRErr, nTTVCR, nTTVCRErr, nWJetsCR,  nWJetsCRErr, nQCDCR, nQCDCRErr, nDYCR, nDYCRErr) = yieldDictCR[bin]
         
-#        allBkgSR = nTTSR + nSingleTSR + nTTVSR + nWJetsSR + nQCDSR + nDYSR
-#        allBkgSRErr = math.sqrt(nTTSRErr*nTTSRErr + nSingleTSRErr*nSingleTSRErr +nTTVSRErr*nTTVSRErr+ nWJetsSRErr*nWJetsSRErr +nQCDSRErr*nQCDSRErr+nDYSRErr*nDYSRErr)
-        allBkgSR = nTTSR + nSingleTSR + nTTVSR + nWJetsSR + nDYSR
-        allBkgSRErr = math.sqrt(nTTSRErr*nTTSRErr + nSingleTSRErr*nSingleTSRErr +nTTVSRErr*nTTVSRErr+ nWJetsSRErr*nWJetsSRErr+nDYSRErr*nDYSRErr)
-#        allBkgSR = 1
-#        if nTTSR_DiLep>0.01:
-#            allBkgSR = nTTSR_DiLep
-
+        allBkgSR = nTTSR + nSingleTSR + nTTVSR + nWJetsSR + nQCDSR + nDYSR
+        allBkgSRErr = math.sqrt(nTTSRErr*nTTSRErr + nSingleTSRErr*nSingleTSRErr +nTTVSRErr*nTTVSRErr+ nWJetsSRErr*nWJetsSRErr +nQCDSRErr*nQCDSRErr+nDYSRErr*nDYSRErr)
         
         allBkgCR = nTTCR + nSingleTCR + nTTVCR + nWJetsCR + nQCDCR + nDYCR
         allBkgCRErr = math.sqrt(nTTCRErr*nTTCRErr + nSingleTCRErr*nSingleTCRErr +nTTVCRErr*nTTVCRErr+ nWJetsCRErr*nWJetsCRErr +nDYCRErr*nDYCRErr) 
-#        allBkgCRErr = math.sqrt(nTTCRErr*nTTCRErr + nSingleTCRErr*nSingleTCRErr +nTTVCRErr*nTTVCRErr+ nWJetsCRErr*nWJetsCRErr +nQCDCRErr*nQCDCRErr+nDYCRErr*nDYCRErr) 
-#        allBkgCR = nTTCR + nSingleTCR + nTTVCR + nWJetsCR + nDYCR
-#        allBkgCR = 1
-#        if nTTCR_DiLep>0.01:
-#            allBkgCR = nTTCR_DiLep
- 
         
         RCS = allBkgSR/allBkgCR
         RCS_Err = RCS * math.sqrt(allBkgSRErr/allBkgSR*allBkgSRErr/allBkgSR + allBkgCRErr/allBkgCR*allBkgCRErr/allBkgCR)
@@ -209,47 +202,154 @@ def makeRCS(yieldDictSR, yieldDictCR):
 
     return RCSval
 
-nameDict = {}
-nameDict['ST1'] = " LT > 250 &&  LT < 350)"                                                                           
-nameDict['ST2'] = " LT > 350  && LT < 450)"
-nameDict['ST3'] = " LT > 450 && LT < 600)"                                                                              
-nameDict['ST4'] = " LT > 600)"
-nameDict['1B'] = "if ((nB == 1) && "
-nameDict['2B'] = "if ((nB == 2) && "
-nameDict['3B'] = "if ((nB >= 3) && "
+binNameDict = {}
+binNameDict['ST1'] = " [250, 350]"                                                                           
+binNameDict['ST2'] = " [350, 450]"
+binNameDict['ST3'] = " [450, 600]"                                                                              
+binNameDict['ST4'] = " $\geq$ 600" 
+binNameDict['ST34'] = " $\geq$ 450" 
+binNameDict['1B'] = " $=$ 1 "
+binNameDict['2B'] = " $=$ 2 "
+binNameDict['3B'] = " $\geq$ 3 "
+binNameDict['23B'] = " $\geq$ 2 "
+binNameDict['123B'] = "$\geq$ 1 "
+binNameDict['HT0'] = " [500, 750]"                                                            
+binNameDict['HT01'] = " [500, 1250]"
+binNameDict['HT1'] = " [750, 1250]"
+binNameDict['HT2'] = " $\geq$ 1250"
+binNameDict['HT12'] = " $\geq$ 750"
+binNameDict['HT012'] = " $\geq$ 500"
+def makeBinTable(binCounts , option = "kfac"):
 
-def convLine(line):          
-    line = line.replace('_45j_HT012','')
-    line = line.replace('_','')
-    for key in nameDict.keys(): 
-        if key in line: 
-            line = ' '+line   
-            line = line.replace(key,nameDict[key]) 
-            cline = line
+    print '\\begin{table}[ht]'
 
-    return cline 
+    binNames = sorted(binCounts.keys())
+    if "68" in binNames[0]:
+        nJetSR = '6, 8'
+    if "9I" in binNames[0]:
+        nJetSR = '9, Inf'
+    if "for6" in binNames[0]:
+        nJetSR = '4, 5 for 6, 8'
+    if "for9" in binNames[0]:
+        nJetSR = '4, 5 for 9, Inf'
 
-def printRCSfunction(RCS_SB):
-    ykeys = sorted(RCS_SB.keys())
-    nkeys = nameDict.keys()
-    for bin in ykeys:
-        print convLine(bin), ' weight = ' + str(RCS_SB[bin][0])+ ';'
-    
+    if option == "kfac":
+        print '\\begin{tabular}{|c|c|c|c|c|c|}'
+        print '\\hline'
+        print '\multicolumn{6}{|c|}{Number of jets: ',nJetSR,'} \\\ \\hline' 
+        print ' $L_T$  & nB & $H_T$ & $RCS_{',nJetSR,'}$ $\pm$ stat. err. & $RCS_{4,5j}$ $\pm$ stat. err. & $\kappa$ $\pm$ stat. err. \\\ '
+        print ' $[$GeV$]$  &  & $[$GeV$]$ & & &  \\\ '
+
+    if option == "counts":
+        print '\\tiny'
+        print '\\begin{tabular}{|l|l|l|c|c|c|c|c|c|c|c|c|c|c|}'
+        print '\\hline'
+        print '\multicolumn{12}{|c|}{Number of jets: ',nJetSR,'} \\\ \\hline' 
+        print '$L_T$ & nB & $H_T$                &    TT (2l) SR    &   TT (rest)SR    &     Rest SR    &     AllBkg SR    &     TT (2l) CR     &    TT (rest) CR    &   Rest CR   &    AllBkg CR &   RCSall \\\  '
+        print '$[$GeV$]$ &  & $[$GeV$]$      &    & &  &    &   &  &  &  & \\\  '
+
+    binsTable = []
+    for binName in binNames:
+        splitName=binName.split("_")
+        B = splitName[0]
+        ST = splitName[1]
+        HT = splitName[3]
+        splitName=[ST,B,HT,(binCounts[binName])]
+        binsTable.append(splitName)
+
+    binsTable= sorted(binsTable)
+    for i,bin in enumerate(binsTable):
+        ST = bin[0]
+        ST0 = ""
+        B = bin[1]
+        B0 = ""
+        HT = bin[2]
+        HT0 = ""
+
+        Bname=B.replace(B,binNameDict[B])
+        STname = ST.replace(ST,binNameDict[ST])
+        HTname =HT.replace(HT,binNameDict[HT])
+
+        if i > 0:
+            ST0=binsTable[i-1][0]
+            B0=binsTable[i-1][1]
+            HT0=binsTable[i-1][2]
+
+        if option == "kfac":
+            RCSSB = bin[3][0]
+            RCSerrSB = bin[3][1]
+            RCSSR = bin[3][2]
+            RCSerrSR = bin[3][3]
+            kappa = bin[3][4]
+            kappaerr = bin[3][5]
+        
+            if ST != ST0:
+                print '\\hline \\hline'
+                print '\\cline{1-6} \multirow{1}{*}{',STname,'}&',Bname, '&', HTname, '&', ' %2.3f' % RCSSB, '$\pm$',' %2.3f' % RCSerrSB, ' &', ' %2.3f' % RCSSR, '$\pm$',' %2.3f' % RCSerrSR,'&',' %2.2f' % kappa, '$\pm$',' %2.2f' % kappaerr,' \\\ '
+            if ST == ST0 and B!=B0:
+                print '\\cline{2-6}' ,'&',Bname, '&', HTname, '&', ' %2.3f' % RCSSB, '$\pm$',' %2.3f' % RCSerrSB, ' &', ' %2.3f' % RCSSR, '$\pm$',' %2.3f' % RCSerrSR,'&',' %2.2f' % kappa, '$\pm$',' %2.2f' % kappaerr,' \\\ '
+            elif ST==ST0 and B==B0:
+                print ' &', '&', HTname, '&',  ' %2.3f' % RCSSB, '$\pm$',' %2.3f' % RCSerrSB, ' &', ' %2.3f' % RCSSR, '$\pm$',' %2.3f' % RCSerrSR,'&',' %2.2f' % kappa, '$\pm$',' %2.2f' % kappaerr,' \\\ '
+
+        if option == "counts":
+
+            nTT_DiLep = bin[3][0]
+            nTT_SemiLep = bin[3][1]
+            allbkgNoTT = bin[3][2]
+            allbkg = bin[3][3]
+            nTT_DiLep2 = bin[3][4]
+            nTT_SemiLep2 = bin[3][5]
+            allbkgNoTT2 = bin[3][6]
+            allbkg2 = bin[3][7]
+            RCSall= bin[3][8]
+
+            if ST != ST0:
+                print '\\hline \\hline'
+                print '\\cline{1-12} \multirow{1}{*}{',STname,'}&',Bname, '&', HTname, '&', ' %2.2f' % nTT_DiLep, ' &', ' %2.2f' % nTT_SemiLep,'&',' %2.2f' % allbkgNoTT, '&',' %2.2f' % allbkg , '&',' %2.2f' % nTT_DiLep2, '&',' %2.2f' % nTT_SemiLep2, '&',' %2.2f' % allbkgNoTT2, '&',' %2.2f' % allbkg2, '&',' %2.4f' % RCSall, ' \\\ '
+            if ST == ST0 and B!=B0:
+                print '\\cline{2-12}' ,'&',Bname, '&', HTname, '&', ' %2.2f' % nTT_DiLep, ' &', ' %2.2f' % nTT_SemiLep,'&',' %2.2f' % allbkgNoTT, '&',' %2.2f' % allbkg , '&',' %2.2f' % nTT_DiLep2, '&',' %2.2f' % nTT_SemiLep2, '&',' %2.2f' % allbkgNoTT2, '&',' %2.2f' % allbkg2, '&',' %2.4f' % RCSall, ' \\\ '
+
+            elif ST==ST0 and B==B0:
+                print ' &', '&', HTname, '&', ' %2.2f' % nTT_DiLep, ' &', ' %2.2f' % nTT_SemiLep,'&',' %2.2f' % allbkgNoTT, '&',' %2.2f' % allbkg , '&',' %2.2f' % nTT_DiLep2, '&',' %2.2f' % nTT_SemiLep2, '&',' %2.2f' % allbkgNoTT2, '&',' %2.2f' % allbkg2, '&',' %2.4f' % RCSall, ' \\\ '
 
 
+        
+
+
+        
+
+    print "\\hline"
+    print "\end{tabular}"
+
+    print "\\end{table}"
+        
 def makeKfactor(RCS_SB, RCS_SR):
+    kfactor = {}
     #doesn't quite work
     #would need to add up bjet multiplicity doesn't quite work yet
-    STbins = ['ST1','ST2','ST3','ST4']
-    names_SB = RCS_SB.keys()
-    names_SR = RCS_SR.keys()
-    for ST in STbins:
-        for name_SB in names_SB:
-            for name_SR in names_SR:
-                if ST in name_SB and ST in name_SR:
-                    print name_SB, name_SR, ST, RCS_SR[name_SR][0]/RCS_SB[name_SB][0]
-                    
+    STbins = ['ST1','ST2','ST3','ST4','ST34']
+    HTbins = ['HT0','HT1','HT2','HT01','HT12','HT012']
+    Bbins = ['1B','3B']
+    names_SB = sorted(RCS_SB.keys())
+    names_SR =  sorted(RCS_SR.keys())
 
+    for name_SR in names_SR:
+        name_SR  = name_SR+'_'
+        splitName=name_SR.split("_")
+        B = splitName[0]
+        if B == "2B" or B == "23B" or B == "3B":
+            B = "23B"
+        ST = splitName[1]
+        HT = splitName[3]+'_'
+        for name_SB in names_SB:
+            if B in name_SB and ST in name_SB and HT in name_SB:
+                #print name_SR,"!!!!!!!!!!!!1"
+                name_SR =  name_SR[:-1]
+                kappa = RCS_SR[name_SR][0]/RCS_SB[name_SB][0]
+                kappaErr = kappa*math.sqrt((RCS_SR[name_SR][1]/RCS_SR[name_SR][0])*(RCS_SR[name_SR][1]/RCS_SR[name_SR][0])+(RCS_SB[name_SB][1]/RCS_SB[name_SB][0]*RCS_SB[name_SB][1]/RCS_SB[name_SB][0]))
+                print "%s %s | %2.4f +/- %2.4f| %2.4f +/- %2.4f| %2.2f +/- %2.2f " %  ( name_SR, name_SB, RCS_SR[name_SR][0], RCS_SR[name_SR][1], RCS_SB[name_SB][0], RCS_SB[name_SB][1], kappa, kappaErr)        
+                kfactor[name_SR] = [RCS_SR[name_SR][0], RCS_SR[name_SR][1],RCS_SB[name_SB][0],RCS_SB[name_SB][1],kappa, kappaErr] 
+    return kfactor
 # MAIN
 if __name__ == "__main__":
 
@@ -294,8 +394,14 @@ if __name__ == "__main__":
     cardNames = [(name.replace(cardPattern+'_','')).replace('.input.root','') for name in cardNames]
 
     SB = '45j'
-    cardNamesSR_SB = [name for name in cardNames if name.find('SR_' + SB) > 0 and (name.find('a') <0 and name.find('012') <0)]
-    cardNamesCR_SB = [name for name in cardNames if name.find('CR_' + SB) > 0 and (name.find('a') <0 and name.find('012') <0)]
+    cardNamesSR_SB_for6 = [name for name in cardNames if name.find('SR_' + SB) > 0 and ( name.find('for6') >0)]
+    cardNamesCR_SB_for6 = [name for name in cardNames if name.find('CR_' + SB) > 0 and (name.find('for6') >0)]
+    cardNamesSR_SB_for9 = [name for name in cardNames if name.find('SR_' + SB) > 0 and ( name.find('for9') >0)]
+    cardNamesCR_SB_for9 = [name for name in cardNames if name.find('CR_' + SB) > 0 and (name.find('for9') >0)]
+
+#    SB = '45j'
+#    cardNamesSR_SB = [name for name in cardNames if name.find('SR_' + SB) > 0 and (name.find('a') <0 and name.find('012') <0)]
+#    cardNamesCR_SB = [name for name in cardNames if name.find('CR_' + SB) > 0 and (name.find('a') <0 and name.find('012') <0)]
 
     SB = '45j'
     cardNamesSR_SBa = [name for name in cardNames if name.find('SR_' + SB) > 0 and (name.find('a') >0 or name.find('012') >0)]
@@ -318,11 +424,17 @@ if __name__ == "__main__":
     cardNamesCR_SB_K = [name for name in cardNames if name.find('CRK_4') > 0]
 
 
-    argTupleSR_SB = [(commondir, cardDirectory, name, ratDict) for name in cardNamesSR_SB]
-    argTupleCR_SB = [(commondir, cardDirectory, name, ratDict) for name in cardNamesCR_SB]
+    argTupleSR_SB_for6 = [(commondir, cardDirectory, name, ratDict) for name in cardNamesSR_SB_for6]
+    argTupleCR_SB_for6 = [(commondir, cardDirectory, name, ratDict) for name in cardNamesCR_SB_for6]
     pool = Pool(nJobs)
-    yieldDictSR_SB = dict(pool.map(_getYieldsFromInput, argTupleSR_SB))             
-    yieldDictCR_SB = dict(pool.map(_getYieldsFromInput, argTupleCR_SB))                   
+    yieldDictSR_SB_for6 = dict(pool.map(_getYieldsFromInput, argTupleSR_SB_for6))             
+    yieldDictCR_SB_for6 = dict(pool.map(_getYieldsFromInput, argTupleCR_SB_for6))                   
+
+    argTupleSR_SB_for9 = [(commondir, cardDirectory, name, ratDict) for name in cardNamesSR_SB_for9]
+    argTupleCR_SB_for9 = [(commondir, cardDirectory, name, ratDict) for name in cardNamesCR_SB_for9]
+    pool = Pool(nJobs)
+    yieldDictSR_SB_for9 = dict(pool.map(_getYieldsFromInput, argTupleSR_SB_for9))             
+    yieldDictCR_SB_for9 = dict(pool.map(_getYieldsFromInput, argTupleCR_SB_for9))                   
 
     argTupleSR_SBa = [(commondir, cardDirectory, name, ratDict) for name in cardNamesSR_SBa]
     argTupleCR_SBa = [(commondir, cardDirectory, name, ratDict) for name in cardNamesCR_SBa]
@@ -355,7 +467,9 @@ if __name__ == "__main__":
     yieldDictCR_SB_K = dict(pool.map(_getYieldsFromInput, argTupleCR_SB_K))                   
 
                             
-    RCS_SB = makeRCS(yieldDictSR_SB,yieldDictCR_SB)
+
+    RCS_SB_for6 = makeRCS(yieldDictSR_SB_for6,yieldDictCR_SB_for6)
+    RCS_SB_for9 = makeRCS(yieldDictSR_SB_for9,yieldDictCR_SB_for9)
     RCS_nj68 = makeRCS(yieldDictSR_nj68,yieldDictCR_nj68)
     RCS_nj9Inf = makeRCS(yieldDictSR_nj9Inf,yieldDictCR_nj9Inf)
 
@@ -363,15 +477,31 @@ if __name__ == "__main__":
     RCS_SB_K = makeRCS(yieldDictSR_SB_K,yieldDictCR_SB_K)
 
 
-    makeTable(yieldDictSR_SB,yieldDictCR_SB,"latex")
-    makeTable(yieldDictSR_SBa,yieldDictCR_SBa,"latex")
-    makeTable(yieldDictSR_K,yieldDictCR_K,"latex")
-    makeTable(yieldDictSR_nj68,yieldDictCR_nj68,"latex")
-    makeTable(yieldDictSR_nj9Inf,yieldDictCR_nj9Inf,"latex")
+    table45_for6 = makeTable(yieldDictSR_SB_for6,yieldDictCR_SB_for6,"latex")
+    table45_for9 = makeTable(yieldDictSR_SB_for9,yieldDictCR_SB_for9,"latex")
+#    makeTable(yieldDictSR_SBa,yieldDictCR_SBa,"latex")
+#    makeTable(yieldDictSR_K,yieldDictCR_K,"latex")
+    table68 = makeTable(yieldDictSR_nj68,yieldDictCR_nj68,"latex")
+    table9Inf = makeTable(yieldDictSR_nj9Inf,yieldDictCR_nj9Inf,"latex")
 
 
 
-#    printRCSfunction(RCS_SB)
+#    printRCSfunction(RCS_SB_for6)
 
     
-#    makeKfactor(RCS_SB_K, RCS_K)
+    kfactor68=    makeKfactor(RCS_SB_for6, RCS_nj68)
+    kfactor9Inf=makeKfactor(RCS_SB_for9, RCS_nj9Inf)
+#    makeBinTable(RCS_nj9Inf)
+#    makeBiTable(RCS_nj68)
+
+    makeBinTable(table45_for6,"counts")
+    makeBinTable(table45_for9,"counts")
+
+    makeBinTable(table68,"counts")
+    makeBinTable(table9Inf,"counts")
+
+    makeBinTable(kfactor68,"kfac")
+    makeBinTable(kfactor9Inf,"kfac")
+
+
+#    makeKfactor(RCS_nj68, RCS_SB_for6)
